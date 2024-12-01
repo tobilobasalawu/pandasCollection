4d311523885e4a2a054f9eb204dcdb27c7127cf2
John Name <oluwatobilobasalawu1234@gmail.com>
2024-12-01 21:39:15 +0000

    creating dataframes without numpy

--- dataFramesPractice.py
+++ dataFramesPractice.py
@@ -4,5 +4,9 @@ import numpy as np #importin numpy module
 
 #to create an df using numpy
 data = np.array([[1, 4], [2,5], [3,6]])
-print(pd.DataFrame(data, index=['row1', 'row2', 'row3'], columns=['col1', 'col2']))
+#print(pd.DataFrame(data, index=['row1', 'row2', 'row3'], columns=['col1', 'col2']))
 
+#creating dataframes without numpy
+data = [['Tobi', 17], ['Harleen', 18], ['Georgia', 18]]
+df = pd.DataFrame(data, columns=['Name', 'Age'], index=['row1', 'row2', 'row3'])
+print(df)
