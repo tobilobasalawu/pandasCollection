791a982a7f758e03d86e6465dbe133050e1797fc
John Name <oluwatobilobasalawu1234@gmail.com>
2024-12-01 21:29:35 +0000

    creating a dataframe using numpy

--- dataFramesPractice.py
+++ dataFramesPractice.py
@@ -2,4 +2,7 @@ import pandas as pd #importing pandas module
 import numpy as np #importin numpy module
 
 
+#to create an df using numpy
+data = np.array([[1, 4], [2,5], [3,6]])
+print(pd.DataFrame(data, index=['row1', 'row2', 'row3'], columns=['col1', 'col2']))
 
