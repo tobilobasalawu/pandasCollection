541488d471ccbd16caa424ce559d6c4e8d9bd3f8
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-07 21:31:01 +0000

    retrieving the math and science column

--- ssData.py
+++ ssData.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 #CSV file
 df = pd.read_csv('student_scores.csv')
-print(df)
+#print(df)
 
 #Extract the value of Science marks for Diana using iloc
-print(df.iloc[3,2])
+#print(df.iloc[3,2])
+
+#Retrieve the Math and Science columns for the first four rows using iloc
+print(df.iloc[:4,[1,2]])
