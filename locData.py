d790be5d3a32c8a3aef41ffaacb728bf940ab34d
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-08 19:42:58 +0000

    updating english column for alice to 95

--- locData.py
+++ locData.py
@@ -2,10 +2,14 @@ import pandas as pd
 df = pd.read_csv("student_scores.csv")
 print(df)
 
+#updating english column for alice to 95
+df.loc[0, 'English'] = 95
+print(df)
+
+
+
+'''
 #all rows where the "Math" column is greater than 85, and only the "Name" and "Math" columns.
 row = df[df.loc[:, 'Math'] > 85]
 print(row.loc[:, ['Name', 'Math']])
-
-'''
-
 '''
