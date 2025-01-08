556625c39b259aec5fec16a22118e390fe0d7d75
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-08 19:40:39 +0000

    Selecting rows where math column is greater than 85 [only 'Name','Math']

--- locData.py
+++ locData.py
@@ -2,9 +2,9 @@ import pandas as pd
 df = pd.read_csv("student_scores.csv")
 print(df)
 
-
-
-
+#all rows where the "Math" column is greater than 85, and only the "Name" and "Math" columns.
+row = df[df.loc[:, 'Math'] > 85]
+print(row.loc[:, ['Name', 'Math']])
 
 '''
 
