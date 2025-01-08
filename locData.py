780993a80a00b0369c9e37153870e3775008e30a
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-08 20:05:49 +0000

    replacing the math value for diana

--- locData.py
+++ locData.py
@@ -2,12 +2,9 @@ import pandas as pd
 df = pd.read_csv("student_scores.csv")
 print(df)
 
-#Find all rows where the "Science" column is between 80 and 90 (inclusive), and display only the "Name" and "Science" columns.
-ans = (df.loc[:, 'Science'] >= 80) & (df.loc[:, 'Science'] <= 90)
-print(df.loc[ans, ['Name', 'Science']])
-
-
-
+#Replace the "Math" value for "Diana" with the sum of her "Science" and "English" scores.
+df.loc[3, 'Math'] = df.loc[3, ['Science', 'English']].sum()
+print(df)
 '''
 #all rows where the "Math" column is greater than 85, and only the "Name" and "Math" columns.
 row = df[df.loc[:, 'Math'] > 85]
@@ -20,4 +17,9 @@ print(df)
 #Create a new column 'Total' which is the sum of the "Math", "Science", and "English" columns, row-wise.
 df['Total'] = df.loc[:, ['Math', 'Science', 'English']].sum(axis=1)
 print(df)
+
+#Find all rows where the "Science" column is between 80 and 90 (inclusive), and display only the "Name" and "Science" columns.
+ans = (df.loc[:, 'Science'] >= 80) & (df.loc[:, 'Science'] <= 90)
+print(df.loc[ans, ['Name', 'Science']])
+
 '''
