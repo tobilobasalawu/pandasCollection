d2da2ba540765df7c5c1fbae85bb935a426486cd
tobilobasalawu <oluwatobilobasalawu1234@gmail.com>
2025-01-10 14:22:07 +0000

    Row Selection with Conditions

--- pandasExcercise.py
+++ pandasExcercise.py
@@ -9,3 +9,9 @@ print(df.iloc[1:4, 1:3].sum())
 
 print(df.loc[df['Math'] > 85, ['Name', 'Math']])
 '''
+
+
+#Row Selection with Conditions
+print(df.iloc[-2:, 2].mean())
+
+print(df.loc[df['English'] > df['Science'], ['Name', 'English', 'Science']])
