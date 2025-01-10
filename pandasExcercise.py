97672375d20f8a572e1c052ffe27302b9e0a702c
tobilobasalawu <oluwatobilobasalawu1234@gmail.com>
2025-01-10 14:18:18 +0000

    row and  column selection

--- pandasExcercise.py
+++ pandasExcercise.py
@@ -1,26 +1,11 @@
 import pandas as pd
-
-df = pd.read_csv("favourite_animals.csv")
-
-'''
-print(df.dtypes) #To display the data types of each columns
-
-print(df['Apples'].mean()) #Average number of apples consumed
-
-print(max(df['Oranges'])) #Max number of oranges consumed
-
-print(df['Bananas'].sum()) #Displaying the total number of bananas consumed
-
-df['Total_Fruits'] = df[['Apples', 'Oranges', 'Bananas']].sum(axis=1)
-print(df)
-
-print(df['Total_Fruits'].mean()) #average of Total_Fruits Column
-
-df.to_csv('Fruits_totals.csv')
-'''
+df = pd.read_csv('student_scores.csv')
 
 print(df)
 
+'''
+#Row and Column Selection 
+print(df.iloc[1:4, 1:3].sum())
 
-
-
+print(df.loc[df['Math'] > 85, ['Name', 'Math']])
+'''
