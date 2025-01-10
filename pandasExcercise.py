ed40d0e9e6b1dcfdde359cd2a18c62ad81415bad
tobilobasalawu <oluwatobilobasalawu1234@gmail.com>
2025-01-10 14:26:51 +0000

    Column Operations

--- pandasExcercise.py
+++ pandasExcercise.py
@@ -8,10 +8,17 @@ print(df)
 print(df.iloc[1:4, 1:3].sum())
 
 print(df.loc[df['Math'] > 85, ['Name', 'Math']])
-'''
-
 
 #Row Selection with Conditions
 print(df.iloc[-2:, 2].mean())
 
 print(df.loc[df['English'] > df['Science'], ['Name', 'English', 'Science']])
+
+'''
+
+#Column Operations
+df['Math'] = df.iloc[:, 1] * 2
+print(df)
+
+df['Average_score'] = df.loc[:, ['Math', 'Science', 'English']].mean(axis=1)
+print(df)
