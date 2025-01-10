f86858790477f88df5ff427929ebd5cea59a2904
tobilobasalawu <oluwatobilobasalawu1234@gmail.com>
2025-01-10 13:47:00 +0000

    Adding a new column and get the average of the Total Fruits column

--- pandasExcercise.py
+++ pandasExcercise.py
@@ -9,3 +9,8 @@ print(df['Apples'].mean()) #Average number of apples consumed
 print(max(df['Oranges'])) #Max number of oranges consumed
 
 print(df['Bananas'].sum()) #Displaying the total number of bananas consumed
+
+df['Total_Fruits'] = df[['Apples', 'Oranges', 'Bananas']].sum(axis=1)
+print(df)
+
+print(df['Total_Fruits'].mean()) #average of Total_Fruits Column
