07259da3ee4b33e029e161280d4fdfbd86d84a4a
tobilobasalawu <oluwatobilobasalawu1234@gmail.com>
2025-01-10 13:41:04 +0000

    operations with fruit consumption

--- pandasExcercise.py
+++ pandasExcercise.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("fruit_consumption.csv")
 
-print(df)
+print(df.dtypes) #To display the data types of each columns
+
+print(df['Apples'].mean()) #Average number of apples consumed
+
+print(max(df['Oranges'])) #Max number of oranges consumed
