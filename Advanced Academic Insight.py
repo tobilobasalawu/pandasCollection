7277b2a874c12895795773dd3acde520a619a7fa
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-11 20:53:59 +0000

    custom visualization

--- Advanced Academic Insight.py
+++ Advanced Academic Insight.py
@@ -4,13 +4,23 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('dataset.csv')
 print(df)
 
+'''
 #Filter and Analyze High Performers
 
 #print(df.loc[(df['Math'] > 85) & (df['English'] > df['Science'])])
 
+'''
 #Aggregate Scores by Subject
 
 average = df[['Math', 'Science', 'English']].mean()
 total = df[['Math', 'Science', 'English']].sum()
 newdf = pd.DataFrame({'Average' : average, 'Total' : total})
-newdf.to_csv('subject_aggregates.csv')
+newdf.to_csv('subject_aggregates.csv', index=False)
+
+#Custom Visualization
+plt.bar(['Math', 'Science', 'English'], newdf['Average'])
+plt.title('Custom Visualization')
+plt.xlabel('Subjects')
+plt.ylabel('Total Scores')
+
+plt.show()
