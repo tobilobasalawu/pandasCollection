7c1b46d62b4f3d2a939b9d37d21a6352ab4354dd
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-11 20:26:18 +0000

    Filtering and analyzing high performers

--- Advanced Academic Insight.py
+++ Advanced Academic Insight.py
@@ -3,3 +3,8 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('dataset.csv')
 print(df)
+
+#Filter and Analyze High Performers
+
+print(df.loc[(df['Math'] > 85) & (df['English'] > df['Science'])])
+
