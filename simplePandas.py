b94f0d48fe6a38856139febe84e3c117eeebb224
John Name <oluwatobilobasalawu1234@gmail.com>
2025-02-03 21:56:52 +0000

    completed program

--- simplePandas.py
+++ simplePandas.py
@@ -11,4 +11,10 @@ def scienceHigh():
   print(df.loc[df['Science'] > 80])
 
 def mathChart():
-  plt.bar(df[''])
+  plt.bar(df['Name'], df['Math'], color='blue')
+  plt.xlabel('Names')
+  plt.ylabel('Scores')
+  plt.title('Student Math Scores')
+  plt.show()
+
+mathChart()
