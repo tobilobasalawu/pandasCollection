4f95ac2d89965d379222433e9e81902d59ae7b36
Tobi <155114173+tobilobasalawu@users.noreply.github.com>
2025-03-02 21:11:08 +0000

    Update ilocData.py

--- randomPractice/ilocData.py
+++ randomPractice/ilocData.py
@@ -43,25 +43,25 @@ a = df[df.iloc[:, 1] % 5 == 0]
 print(a.iloc[:, 1])
 
 #Extract the value of Science marks for Diana using iloc
-#print(df.iloc[3,2])
+print(df.iloc[3,2])
 
 #Retrieve the Math and Science columns for the first four rows using iloc
-#print(df.iloc[:4,[1,2]])
+print(df.iloc[:4,[1,2]])
 
 #Find the sum of all English marks using iloc
-#print(df.iloc[:, 3].sum())
+print(df.iloc[:, 3].sum())
 
 #Replace the Science score of Charlie with 80 using iloc.
-#ans = df.iloc[2,2] = 80
-#print(df)
+ans = df.iloc[2,2] = 80
+print(df)
 
 #Find the mean of the Math scores for the first three students using iloc.
-#print(df.iloc[:3, 1].mean())
+print(df.iloc[:3, 1].mean())
 
 #Extract the rows for Alice and Bob and print only their English scores using
-#print(df.iloc[:2, 3])
+print(df.iloc[:2, 3])
 
 #ans = 
-#print(ans)
-#df['Total'] = df.iloc[:,[1,2,3]].sum(axis=1)
-#print(df)
+print(ans)
+df['Total'] = df.iloc[:,[1,2,3]].sum(axis=1)
+print(df)
