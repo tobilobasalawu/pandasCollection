22700eb2932ff40644d801d3f9e4fe3194fc9957
Tobi <oluwatobilobasalawu1234@gmail.com>
2025-03-12 22:50:46 +0000

    feat: Added comments

--- examPractice/Task4a/Task4a_Elanp_dashboard_November 22.py
+++ examPractice/Task4a/Task4a_Elanp_dashboard_November 22.py
@@ -1,8 +1,13 @@
-from operator import index
-
-import pandas as pd
+import pandas as pd #Importing data handling library
 
 def main_menu():
+
+    '''
+    The main menu - that is first displayed to the user and prompts the user for a choice
+    :returns:
+        str: The user choice
+    '''
+
     flag = True
 
     while flag:
@@ -30,8 +35,14 @@ def main_menu():
 
     return choice
 
-
+#
 def get_depart():
+    '''
+       Handles the users input for departure Airport - user enters a choice
+       :returns:
+           str: The user choice
+       '''
+
     flag = True
 
     while flag:
@@ -60,6 +71,12 @@ def get_depart():
 
 
 def get_destination(depart):
+    '''
+       Handles the users input for destination Airport - user enters a choice
+       :returns:
+           str: The user choice
+       '''
+
     flag = True
 
     while flag:
@@ -96,7 +113,13 @@ def get_destination(depart):
 
     return choice
 
+
 def get_number_days():
+    '''
+       Handles the number of days selected by the user - user enters a choice
+       :returns:
+           str: The user choice
+       '''
 
     flag = True
 
@@ -120,8 +143,13 @@ def get_number_days():
     return int(choice)
 
 
-
 def convert_men_choice(choice):
+    '''
+       Converts the users input from integer to the accepted airport values
+       :returns:
+           str: The converted choice
+       '''
+
     if choice == "1":
         conv_choice = "DUB"
         return conv_choice
@@ -141,9 +169,17 @@ def convert_men_choice(choice):
         conv_choice =  "MAN"
         return conv_choice
 
-
 def get_data( depart, dest,days):
-    df = pd.read_csv("Task4a_data.csv")
+    '''
+       Gets the number of days selected by the user based on the departure and destination airport
+       :returns:
+           str: The extracted days
+       '''
+    try:
+        df = pd.read_csv('Task4a_data.csv')
+    except FileNotFoundError:
+        print(' ')
+        print('Data file cannot be found')
     extract = df.loc[(df['From'] == depart) & (df['To'] == dest)]
     extract_days = extract.iloc[: , -days: ]
     print("We have found these flights that match your criteria:")
@@ -151,7 +187,18 @@ def get_data( depart, dest,days):
     return extract_days
 
 def departure_airport_that_has_the_most_passengers_over_time():
-    df = pd.read_csv('Task4a_data.csv')
+    '''
+       Gets the departure airport with the most passengers over time
+       :returns:
+           A dataframe showing the most passengers on each date for the departure airport
+           str: The departure airport that has the most passengers over time
+       '''
+
+    try:
+        df = pd.read_csv('Task4a_data.csv')
+    except FileNotFoundError:
+        print(' ')
+        print('Data file cannot be found')
 
     date_columns_only = df.iloc[:, 3:]
     departure_airport_and_date_columns = df.groupby('From')[date_columns_only.columns].sum()
@@ -166,16 +213,33 @@ def departure_airport_that_has_the_most_passengers_over_time():
     print(f"The departure airport that has the most passengers over time is {departure_airport_most_passengers_over_time}, with a whooping passengers number of {departure_airport_most_passengers_over_time_values}")
 
 
-def AM_and_PM_flights_for_two_selected_routes(depart_route_1, destination_route_1, depart_route_2, destination_route_2, time):
-    df = pd.read_csv('Task4a_data.csv')
+def AM_and_PM_flights_for_two_selected_routes(depart_route_1, destination_route_1, depart_route_2, destination_route_2):
+    '''
+       Gets the AM and PM for the selected routes
+       :returns:
+           A dataframe showing the details and time for the two selected routes
+       '''
+    try:
+        df = pd.read_csv('Task4a_data.csv')
+    except FileNotFoundError:
+        print(' ')
+        print('Data file cannot be found')
 
     selected_routes = df.loc[((df['From'] == depart_route_1) & (df['To'] == destination_route_1)) |  ((df['From'] == depart_route_2) & (df['To'] == destination_route_2))]
 
-    time_selected_routes = selected_routes.loc[selected_routes['Time'] == time]
-    print(f"The flights for the selected routes are: \n\n{time_selected_routes.to_string(index=False)}")
+    time_selected_routes_AM = selected_routes.loc[selected_routes['Time'] == 'AM']
+    time_selected_routes_PM = selected_routes.loc[selected_routes['Time'] == 'PM']
+    print(f"The flights of the selected routes for AM are: \n\n{time_selected_routes_AM.to_string(index=False)}")
+    print(' ')
+    print(f"The flights of the selected routes for PM are: \n\n{time_selected_routes_PM.to_string(index=False)}")
+    print(' ')
 
 
 def run_program():
+    '''
+       Handles the execution of the entire program
+    '''
+
     main_menu_choice = main_menu()
 
     if main_menu_choice == '1':
@@ -228,7 +292,7 @@ def run_program():
                 else:
                     flag = False
                     print('Choice Accepted!\n')
-                    AM_and_PM_flights_for_two_selected_routes(convert_men_choice(str(depart1)), convert_men_choice(str(des1)), convert_men_choice(str(depart2)), convert_men_choice(str(des2)), 'AM')
+                    AM_and_PM_flights_for_two_selected_routes(convert_men_choice(str(depart1)), convert_men_choice(str(des1)), convert_men_choice(str(depart2)), convert_men_choice(str(des2)))
             except:
                 print('Please enter the correct option\n')
 
