d32cdd88e6938466d30a46603b5c497b0ef69267
Tobi <oluwatobilobasalawu1234@gmail.com>
2025-03-13 20:45:39 +0000

    change: fixed errors for JPY to GBP in CSV file and added whitespace/blank lines

--- examPractice/ESP_SAM/Task 4A/Task4a_currency_conversion.py
+++ examPractice/ESP_SAM/Task 4A/Task4a_currency_conversion.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 #The menu() function generates the UI the accepts and validates user choice
 def menu():
 
@@ -15,11 +14,14 @@ def menu():
         print("4. Austrailan Dollars (AUD) to Pound Sterling (GBP)")
         print("5. Pound Sterling (GBP) to Japanese Yen (JPY)")
         print("6. Japanese Yen (JPY) to Pound Sterling (GBP)")
-        print("")
+        print("7. Pound Sterling (GBP) to US Dollar (USD)")
+        print("8. US Dollar (USD) to Pound Sterling (GBP)")
+        print(' ')
         print("######################################################")
 
         
         menu_choice = input("Please enter the number of your choice (1-6): ")
+        print(' ')
 
         try:
             int(menu_choice)
@@ -27,8 +29,8 @@ def menu():
             print("Sorry, you did not enter a valid choice")
             flag = True
         else:
-            if int(menu_choice) < 1 or int(menu_choice) > 6:
-                print("Sorry, you did not neter a valid choice")
+            if int(menu_choice) < 1 or int(menu_choice) > 8:
+                print("Sorry, you did not enter a valid choice")
                 flag = True
             else:
                 return menu_choice  
@@ -37,12 +39,15 @@ def menu():
 #Gets the short version of the conversion information based on user menu choice
 def get_currency ():
     currencies = {
-       '1': 'GBP - EUR',
-       '2': 'EUR - GBP', 
-       '3': 'GBP - AUD',
-       '4': 'AUD - GBP',
-       '5': 'GPB - JPY',
-       '6': 'JPY - GBP'}
+        '1': 'GBP - EUR',
+        '2': 'EUR - GBP',
+        '3': 'GBP - AUD',
+        '4': 'AUD - GBP',
+        '5': 'GPB - JPY',
+        '6': 'JPY - GBP',
+        '7': 'GBP - USD',
+        '8': 'USD - GBP'
+    }
    
     currency = currencies.get(menu_choice)
     
@@ -74,11 +79,12 @@ def get_amount_to_convert():
     flag = True
     
     while flag:
-        conversion_amount = input("please enter the ammount you wish to convert")
+        conversion_amount = input("Please enter the amount you wish to convert: ")
     
         try:
             float(conversion_amount)
         except:
+            print(' ')
             print("Sorry, you must enter a numerical value")
             flag = True
         else:
@@ -90,8 +96,9 @@ conversion_amount = float(get_amount_to_convert())
 def perfom_conversion():
     amount_recieved = round(conversion_amount * conversion_rate, 2)
 
+    print(' ')
     print("##################################")
     print('You are converting {} in {}'.format(conversion_amount, currency[0:3]) )
-    print('You will recieve {} in {}'.format(amount_recieved, currency[6:9]))
+    print('You will receive {} in {}'.format(amount_recieved, currency[6:9]))
     
 perfom_conversion()
