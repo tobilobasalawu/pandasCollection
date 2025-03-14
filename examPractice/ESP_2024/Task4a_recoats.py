49eaa44c12cf3ad5d6a134f170289f20739cf5f4
Tobi <oluwatobilobasalawu1234@gmail.com>
2025-03-14 22:09:46 +0000

    feat: added ESP 2024 Task 4a

---
+++ examPractice/ESP_2024/Task4a_recoats.py
@@ -0,0 +1,89 @@
+import pandas as pd
+
+# Outputs the initial menu and checks validates the input
+def main_menu():
+    flag = True
+
+    while flag:
+
+        print("####################################################")
+        print("############## Recoats Adventure Park ##############")
+        print("####################################################")
+        print("")
+        print("########### Please select an option ################")
+        print("### 1. Total income by source")
+
+        choice = input('Enter your number selection here: ')
+
+        try:
+            int(choice)
+        except:
+            print("Sorry, you did not enter a valid option")
+            flag = True
+        else:    
+            print('Choice accepted!')
+            flag = False
+
+    return choice
+
+# Submenu for totals, provides type check validation for the input
+def total_menu ():
+    flag = True
+
+    while flag:
+
+        print("####################################################")
+        print("############## Total income by source ##############")
+        print("####################################################")
+        print("")
+        print("########## Please select an income source ##########")
+        print("### 1. Tickets")   
+        print("### 2. Gift Shop") 
+        print("### 3. Snack Stand")  
+        print("### 4. Pictures")
+
+        choice = input('Enter your number selction here: ')
+
+        try:
+            int(choice)
+        except:
+            print("Sorry, you did not enter a valid option")
+            flag = True
+        else:    
+            print('Choice accepted!')
+            flag = False
+
+    return choice   
+
+# takes the total submenu input and converts the number to a string of the source name
+def convert_total_men_coice(total_men_choice):
+    
+    if total_men_choice == "1":
+        tot_choice = "Tickets"
+    elif total_men_choice == "2":
+        tot_choice = "Gift Shop"
+    elif total_men_choice == "3":
+        tot_choice= "Snack Stand"
+    else:
+        tot_choice = "Pictures"  
+    
+    return tot_choice
+
+# creates a new dataframe with the selected income source then creates a total row
+# outputs the final total in a message
+def get_total_data(total_choice):
+    
+    df = pd.read_csv("Task4a_data.csv")
+    
+    income = df[["Day", total_choice]]
+
+    total = income[total_choice].sum()
+
+    msg = "The total income from {} was: £{}".format(total_choice, total)
+    return msg
+
+main_menu_choice = main_menu()
+if main_menu_choice == "1":
+    total_men_choice = total_menu()
+    total_choice = convert_total_men_coice(total_men_choice)
+    print(get_total_data(total_choice))
