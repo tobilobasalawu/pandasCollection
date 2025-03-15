34d2326fb9b01580ff44f0f9411bd8c1969090c2
Tobi <oluwatobilobasalawu1234@gmail.com>
2025-03-15 00:28:05 +0000

    feat: implemented functions from new solution into main program

--- examPractice/ESP_2024/Task4a_recoats.py
+++ examPractice/ESP_2024/Task4a_recoats.py
@@ -6,13 +6,15 @@ def main_menu():
 
     while flag:
 
-        print("####################################################")
-        print("############## Recoats Adventure Park ##############")
-        print("####################################################")
-        print("")
-        print("########### Please select an option ################")
-        print("### 1. Total income by source")
-        print(' ')
+        print("""
+            ####################################################
+            ############## Recoats Adventure Park ##############
+            ####################################################
+            ########### Please select an option ################
+            ### 1. Total income by source
+            ### 2. View the different payment types and income sources
+            ### 3. View the income on different days of the week
+        """)
 
         choice = input('Enter your number selection here: ')
 
@@ -24,6 +26,7 @@ def main_menu():
             flag = True
         else:    
             print('Choice accepted!')
+            print(' ')
             flag = False
 
     return choice
@@ -33,18 +36,17 @@ def total_menu ():
     flag = True
 
     while flag:
-        print(' ')
         print("####################################################")
         print("############## Total income by source ##############")
         print("####################################################")
-        print("")
+        print(" ")
         print("########## Please select an income source ##########")
         print("### 1. Tickets")   
         print("### 2. Gift Shop") 
         print("### 3. Snack Stand")  
         print("### 4. Pictures")
 
-        choice = input('Enter your number selction here: ')
+        choice = input('Enter your number selection here: ')
 
         try:
             int(choice)
@@ -85,7 +87,8 @@ def get_total_data(total_choice):
     msg = "The total income from {} was: £{}".format(total_choice, total)
     return msg
 
-def different_payment_types_and_income_sources():
+
+def different_payment_types_and_income_sources(user_payment_type_input):
     df = pd.read_csv("Task4a_data.csv")
 
     different_payment_types_and_income_sources_values = df.groupby('Pay Type').agg({
@@ -95,8 +98,13 @@ def different_payment_types_and_income_sources():
         'Pictures': 'sum'
     })
 
-    print('The different payment types and income sources over time is: \n')
-    print(different_payment_types_and_income_sources_values)
+    print(f'\nThe Income sources for {user_payment_type_input} overtime is:')
+    if user_payment_type_input.capitalize() == 'Card':
+        print(different_payment_types_and_income_sources_values.iloc[0])
+    else:
+        print(different_payment_types_and_income_sources_values.iloc[1])
+
+
 
 def income_on_different_days_of_the_week():
     df = pd.read_csv("Task4a_data.csv")
@@ -110,11 +118,32 @@ def income_on_different_days_of_the_week():
 
     income_sources_on_different_days_values['Total'] = income_sources_on_different_days_values.sum(axis=1)
 
-    print('The income on the different days of the week over time is: \n')
+    print('\nThe income on the different days of the week over time is:')
     print(income_sources_on_different_days_values)
 
-main_menu_choice = main_menu()
-if main_menu_choice == "1":
-    total_men_choice = total_menu()
-    total_choice = convert_total_men_coice(total_men_choice)
-    print(get_total_data(total_choice))
+
+def execute_main_program():
+    main_menu_choice = main_menu()
+
+    flag = True
+
+    while flag:
+        if main_menu_choice == "1":
+            total_men_choice = total_menu()
+            total_choice = convert_total_men_coice(total_men_choice)
+            print(get_total_data(total_choice))
+            flag = False
+        elif main_menu_choice == "2":
+            user_payment_type_input = input("Enter the Payment type you would view: ").capitalize()
+            if user_payment_type_input in {'Card', 'Cash'}:
+                different_payment_types_and_income_sources(user_payment_type_input)
+                flag = False
+            else:
+                print('The Payment type you entered is not valid, Try Again!\n')
+        elif main_menu_choice == "3":
+            income_on_different_days_of_the_week()
+            flag = False
+        else:
+            print('The choice you selected is not in the menu, Try Again!')
+
+execute_main_program()
